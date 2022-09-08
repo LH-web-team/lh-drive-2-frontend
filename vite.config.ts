2ab671b9abf11f06bee0ecb6e487e717fd3fb7d9
StevenH <stevenhong2623@gmail.com>
2022-09-09 01:46:08 +0800

    element plus auto import config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
@@ -11,9 +14,15 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
     Pages({
       extendRoute(route, parent) {
-        if (route.path === "/login" || route.path === "/") {
+        if (route.path === "/login" || route.path === "/" || route.path == "/:fileId") {
           return route;
         }
 
