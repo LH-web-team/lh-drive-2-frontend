8c8146fcfa97d6cd40262a22d390421c723d45fd
StevenH <stevenhong2623@gmail.com>
2022-09-09 23:16:06 +0800

    add file path compoent

---
+++ src/components/file/FilePath.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const path = ["My Files", "Folder A", "Folder B", "Folder C"];
+</script>
+
+<template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item v-for="folder in path">
+      <el-button size="large" text>{{ folder }}</el-button>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
--- src/pages/[fileId]/index.vue
+++ src/pages/[fileId]/index.vue
@@ -1,2 +1,13 @@
-<script lang="ts" setup></script>
-<template></template>
+<script lang="ts" setup>
+import ActionBar from "../../components/file/ActionBar.vue";
+import FilePath from "~/components/file/FilePath.vue";
+</script>
+<template>
+  <div class="flex flex-col w-full">
+    <action-bar></action-bar>
+    <hr />
+    <div class="px-6 py-3 mt-6">
+      <file-path></file-path>
+    </div>
+  </div>
+</template>
