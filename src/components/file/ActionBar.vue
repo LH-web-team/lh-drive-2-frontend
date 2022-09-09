9ad75c59dfe58dcd2ee3523890b56e818fab4fd7
StevenH <stevenhong2623@gmail.com>
2022-09-09 23:15:28 +0800

    add action bar to file page

---
+++ src/components/file/ActionBar.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="h-14 flex px-6 py-3 w-full">
+    <el-button type="primary">
+      <el-icon :size="20" class="align-middle"><i-ic-baseline-plus /></el-icon>
+      <span class="font-semibold align-middle">Create</span>
+    </el-button>
+
+    <el-button text>
+      <el-icon :size="20" class="align-middle"><i-ic-outline-file-upload /></el-icon>
+      <span class="font-semibold align-middle">Upload</span>
+    </el-button>
+
+    <el-button text>
+      <el-icon :size="20" class="align-middle"><i-ic-outline-share /></el-icon>
+      <span class="font-semibold align-middle">Share</span>
+    </el-button>
+  </div>
+</template>
